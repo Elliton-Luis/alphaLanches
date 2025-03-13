b39b2a3b768e0465609d5c49783b254143062c7a
ArthurThuko <arthurhenriqsfc@gmail.com>
2025-03-13 16:20:01 -0300

    Criação de rota para tela de perfil

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,19 @@ use App\Http\Middleware\verifyAdmin;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\CreateUserController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/home', [HomeController::class, 'showHome'])->name('home.index');
 
 Route::get('/', [LoginController::class, 'showLogin'])->name('login.index');
 Route::post('/auth', [LoginController::class, 'authUser'])->name('login.auth');
 Route::get('/logout', [LoginController::class, 'logoutUser'])->name('login.logout');
+
+#Rota para a tela de perfil do usuário
+Route::middleware(['auth'])->group(function () {
+    Route::get('/perfil', [ProfileController::class, 'index'])->name('perfil.index');
+    Route::post('/perfil', [ProfileController::class, 'update'])->name('perfil.update');
+});
 
 Route::get('/create/user', [CreateUserController::class, 'showIndex'])->name('create.user.index')->middleware(verifyAdmin::class);
 
