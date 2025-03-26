74fef46749964ab4ffdd8890588773cc830167e8
ArthurThuko <arthurhenriqsfc@gmail.com>
2025-03-26 10:28:26 -0300

    Modificação das rotas do sidebar e do painel home

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,5 +1,4 @@
 <div id="sidebar" class="collapse d-none d-md-flex flex-column flex-shrink-0 p-3 text-bg-dark" style="width: 240px;">
-<<<<<<< HEAD
     <div id="image" style="margin-left: 15%">
         <a href="{{ url('home') }}" class="d-flex align-items-center" style="width: 100px">
             <img src="{{ asset('images/AlphaLanches-Logo.png') }}" height=78px" alt="Logo Alpha">
@@ -30,74 +29,6 @@
                 </a>
             </li>
         @endforeach
-=======
-    <a href="{{route('home.index')}}" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-        <img src="{{ asset('images/logo_black.png') }}" height="30px" alt="Logo Alpha">
-    </a>
-    <hr>
-    
-    <ul class="nav nav-pills flex-column mb-auto">
-    <ul class="nav flex-column">
-
-    <li class="nav-item">
-        <a href="{{route('home.index')}}" class="nav-link text-white btn btn-primary text-start">
-            <i class="bi bi-house-door-fill fs-5 me-2"></i> Painel Inicial
-        </a>
-    </li>
-
-    <li class="nav-item">
-        <a href="#" class="nav-link text-white btn btn-primary text-start">
-            <i class="bi bi-person-fill-add fs-5 me-2"></i> Cadastrar Usuário
-        </a>
-    </li>
-
-    <li class="nav-item">
-        <a href="#" class="nav-link text-white btn btn-primary text-start">
-            <i class="bi bi-currency-dollar fs-5 me-2"></i> Financeiro
-        </a>
-    </li>
-
-    <li class="nav-item">
-        <a href="{{route('estoque.index')}}" class="nav-link text-white btn btn-primary text-start">
-            <i class="bi bi-box fs-5 me-2"></i> Estoque
-        </a>
-    </li>
-
-    <li class="nav-item">
-        <a href="#" class="nav-link text-white btn btn-primary text-start">
-            <i class="bi bi-person-circle fs-5 me-2"></i> Perfil
-        </a>
-    </li>
-
-    <li class="nav-item">
-        <a href="#" class="nav-link text-white btn btn-primary text-start">
-            <i class="bi bi-people-fill fs-5 me-2"></i> Usuários
-        </a>
-    </li>
-
-    <li class="nav-item">
-        <a href="#" class="nav-link text-white btn btn-primary text-start">
-            <i class="bi bi-basket fs-5 me-2"></i> Histórico de Compras
-        </a>
-    </li>
-
-    <li class="nav-item">
-        <a href="#" class="nav-link text-white btn btn-primary text-start">
-            <i class="bi bi-shop fs-5 me-2"></i> PDV
-        </a>
-    </li>
-
-    <li class="nav-item">
-        <a href="#" class="nav-link text-white btn btn-primary text-start">
-            <i class="bi bi-info-circle fs-5 me-2"></i> Sobre Nós
-        </a>
-    </li>
-</ul>
-
-</ul>
-
-            
->>>>>>> c5ad58bbeca4a1f9043f6e0404e65b8d181a2c46
     </ul>
 
     <hr>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,6 @@
         <h2 class="mb-4">Grade de Painéis</h2>
 
         <div class="row row-cols-2 row-cols-md-3 row-cols-lg-4 g-3">
-<<<<<<< HEAD
             @php
                 $buttons = [
                     ['route' => 'financeiro', 'icon' => 'cash-coin', 'label' => 'Financeiro'],
@@ -31,58 +30,6 @@
                     </button>                    
                 </div>
             @endforeach
-=======
-        <div class="col">
-    <button class="btn btn-light square-btn" onclick="redirectTo('painelFinanceiro')">
-        <i class="fas fa-money-bill-trend-up icon-size"></i>
-        <div class="label-size">Financeiro</div>
-    </button>
-</div>
-
-<div class="col">
-    <button class="btn btn-light square-btn" onclick="redirectTo('estoque')">
-        <i class="fas fa-box icon-size"></i>
-        <div class="label-size">Estoque</div>
-    </button>
-</div>
-
-<div class="col">
-    <button class="btn btn-light square-btn" onclick="redirectTo('profile')">
-        <i class="fas fa-user icon-size"></i>
-        <div class="label-size">Perfil</div>
-    </button>
-</div>
-
-<div class="col">
-    <button class="btn btn-light square-btn" onclick="redirectTo('painelUsuarios')">
-        <i class="fas fa-users icon-size"></i>
-        <div class="label-size">Usuários</div>
-    </button>
-</div>
-
-<div class="col">
-    <button class="btn btn-light square-btn" onclick="redirectTo('painelCompras')">
-        <i class="fas fa-basket-shopping icon-size"></i>
-        <div class="label-size">Histórico de Compras</div>
-    </button>
-</div>
-
-<div class="col">
-    <button class="btn btn-light square-btn" onclick="redirectTo('painelPDV')">
-        <i class="fas fa-store icon-size"></i>
-        <div class="label-size">PDV</div>
-    </button>
-</div>
-
-<div class="col">
-    <button class="btn btn-light square-btn" onclick="redirectTo('sobre')">
-        <i class="fas fa-info-circle icon-size"></i>
-        <div class="label-size">Sobre Nós</div>
-    </button>
-</div>
-
-
->>>>>>> c5ad58bbeca4a1f9043f6e0404e65b8d181a2c46
         </div>
     </div>
 @endsection
