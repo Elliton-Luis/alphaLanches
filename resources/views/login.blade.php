971fbdc44d96684195cd49c2e272c7dbc085d6d6
ArthurThuko <arthurhenriqsfc@gmail.com>
2025-03-26 10:47:02 -0300

    Modificações no title da página e add a logo

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,12 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body class="d-flex justify-content-center align-items-center vh-100 bg-light">
 
     <div class="card p-4 shadow" style="width: 350px;">
+        <div class="d-flex justify-content-center align-items-center">
+            <img src="{{ asset('images/AlphaLanches-Logo.png') }}" width="50%" alt="Logo Alpha">
+        </div>
+
         <h3 class="text-center mb-3">Login</h3>
 
         @if(Session::has('errorAuth'))
