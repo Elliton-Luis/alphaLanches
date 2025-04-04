9c88051a1ebbca29ecff5035993eb13b4922be2b
ArthurThuko <arthurhenriqsfc@gmail.com>
2025-04-04 11:03:45 -0300

    Modificação no blade esqueciSenha

--- resources/views/esqueciSenha.blade.php
+++ resources/views/esqueciSenha.blade.php
@@ -1,9 +1,27 @@
-@extends('layouts.default')
+<!DOCTYPE html>
+<html lang="pt-br">
 
-@section('title', 'AlphaLanches - Esqueci Senha')
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>AlphaLanches - Esqueci Senha</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 
-@section('content')
-    <script src="{{ asset('js/financeiro.js') }}"></script>
+</head>
 
-    
-@endsection
+<body class="d-flex justify-content-center align-items-center vh-100 bg-light">
+
+    <div class="card p-4 shadow" style="width: 350px;">
+        <div class="d-flex justify-content-center align-items-center">
+            <img src="{{ asset('images/AlphaLanches-Logo.png') }}" width="50%" alt="Logo Alpha">
+        </div>
+
+        <h3 class="text-center mb-3">Esqueci Senha</h3>
+
+        
+</body>
+
+</html>
