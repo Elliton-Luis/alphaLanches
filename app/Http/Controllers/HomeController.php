0b1df67413c82b54c665992926ce5caf2c7b36b8
Elliton luís <ellitonjunior05@gmail.com>
2025-04-08 14:56:35 -0300

    Botões da home colocados no homeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,57 +7,48 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function showHome(){
-        $buttons = [
-            ['route' => 'financeiro', 'icon' => 'cash-coin', 'label' => 'Financeiro'],
-            ['route' => 'estoque', 'icon' => 'box', 'label' => 'Estoque'],
-            ['route' => 'profile', 'icon' => 'person', 'label' => 'Perfil'],
-            ['route' => 'painelUsuarios', 'icon' => 'people', 'label' => 'Usuários'],
-            ['route' => 'painelCompras', 'icon' => 'basket3', 'label' => 'Histórico de Compras'],
-            ['route' => 'painelPDV', 'icon' => 'shop', 'label' => 'PDV'],
-            ['route' => 'sobre', 'icon' => 'info-circle', 'label' => 'Sobre Nós'],
-        ];
+        if(auth()->user()->type == 'admin')
+            {
+                $buttons = [
+                    ['route' => 'financeiro', 'icon' => 'cash-coin', 'label' => 'Financeiro'],
+                    ['route' => 'estoque', 'icon' => 'box', 'label' => 'Estoque'],
+                    ['route' => 'profile', 'icon' => 'person', 'label' => 'Perfil'],
+                    ['route' => 'painelUsuarios', 'icon' => 'people', 'label' => 'Usuários'],
+                    ['route' => 'painelCompras', 'icon' => 'basket3', 'label' => 'Histórico de Compras'],
+                    ['route' => 'painelPDV', 'icon' => 'shop', 'label' => 'PDV'],
+                    ['route' => 'sobre', 'icon' => 'info-circle', 'label' => 'Sobre Nós'],
+                    ];
+            }
+            elseif (auth()->user()->type == 'func')
+            {
+                $buttons = [
+                    ['route' => 'estoque', 'icon' => 'box', 'label' => 'Estoque'],
+                    ['route' => 'profile', 'icon' => 'person', 'label' => 'Perfil'],
+                    ['route' => 'painelCompras', 'icon' => 'basket3', 'label' => 'Histórico de Compras'],
+                    ['route' => 'painelPDV', 'icon' => 'shop', 'label' => 'PDV'],
+                    ['route' => 'sobre', 'icon' => 'info-circle', 'label' => 'Sobre Nós'],
+                    ];
+            }
+
+            elseif (auth()->user()->type == 'guard')
+            {
+                $buttons = [
+                    ['route' => 'profile', 'icon' => 'person', 'label' => 'Perfil'],
+                    ['route' => 'recarga', 'icon' => 'wallet', 'label' => 'Recarga'],
+                    ['route' => 'sobre', 'icon' => 'info-circle', 'label' => 'Sobre Nós'],
+                ];
+            }
+
+            elseif (auth()->user()->type == 'student')
+            {
+                $buttons = [
+                    ['route' => 'profile', 'icon' => 'person', 'label' => 'Perfil'],
+                    ['route' => 'recarga', 'icon' => 'wallet', 'label' => 'Recarga'],
+                    ['route' => 'PainelHistorico', 'icon' => 'basket3', 'label' => 'Histórico de Compras'],
+                    ['route' => 'sobre', 'icon' => 'info-circle', 'label' => 'Sobre Nós'],
+                ];
+            }
         return view('home',['buttons' => $buttons]);
     }
-    public function homeAdmin(){
-        $buttons = [
-            ['route' => 'financeiro', 'icon' => 'cash-coin', 'label' => 'Financeiro'],
-            ['route' => 'estoque', 'icon' => 'box', 'label' => 'Estoque'],
-            ['route' => 'profile', 'icon' => 'person', 'label' => 'Perfil'],
-            ['route' => 'painelUsuarios', 'icon' => 'people', 'label' => 'Usuários'],
-            ['route' => 'painelCompras', 'icon' => 'basket3', 'label' => 'Histórico de Compras'],
-            ['route' => 'painelPDV', 'icon' => 'shop', 'label' => 'PDV'],
-            ['route' => 'sobre', 'icon' => 'info-circle', 'label' => 'Sobre Nós'],
-        ];
-        return $buttons;
-    }
-
-    public function homeFunc(){
-        $buttons = [
-            ['route' => 'estoque', 'icon' => 'box', 'label' => 'Estoque'],
-            ['route' => 'profile', 'icon' => 'person', 'label' => 'Perfil'],
-            ['route' => 'painelCompras', 'icon' => 'basket3', 'label' => 'Histórico de Compras'],
-            ['route' => 'painelPDV', 'icon' => 'shop', 'label' => 'PDV'],
-            ['route' => 'sobre', 'icon' => 'info-circle', 'label' => 'Sobre Nós'],
-        ];
-        return $buttons;
-    }
-
-    public function homeStudent(){
-        $buttons = [
-            ['route' => 'profile', 'icon' => 'person', 'label' => 'Perfil'],
-            ['route' => 'recarga', 'icon' => 'wallet', 'label' => 'Recarga'],
-            ['route' => 'PainelHistorico', 'icon' => 'basket3', 'label' => 'Histórico de Compras'],
-            ['route' => 'sobre', 'icon' => 'info-circle', 'label' => 'Sobre Nós'],
-        ];
-        return $buttons;
-    }
-
-    public function homeGuard(){
-        $buttons = [
-            ['route' => 'profile', 'icon' => 'person', 'label' => 'Perfil'],
-            ['route' => 'recarga', 'icon' => 'wallet', 'label' => 'Recarga'],
-            ['route' => 'sobre', 'icon' => 'info-circle', 'label' => 'Sobre Nós'],
-        ];
-        return $buttons;
-    }
+    
 }
--- resources/views/components/editableButton.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="col">
-        <a href="{{$route}}" class="btn btn-light square-btn">
-            <i class="bi bi-{{$icon}}"></i>
-            <span>{{$name}}</span>
-        </a>
-    </div>
