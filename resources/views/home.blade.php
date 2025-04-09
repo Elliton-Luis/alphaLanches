9b509264675e33223db89cc63bfee1713d29e26f
Lucas <lucaserpt@gmail.com>
2025-04-09 14:18:34 -0300

    Melhorias de responsividade e estilo nas páginas Home, Recarga e Tabela de Contas

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,13 +8,13 @@
 
     <div class="container mt-4 text-center">
         <h2 class="mb-4">Grade de Painéis</h2>
-        <div class="row row-cols-2 row-cols-md-3 row-cols-lg-4 g-3">
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-3">
             @foreach ($buttons as $button)
                 <div class="col">
-                    <button class="btn btn-light square-btn" data-route="{{ url($button['route']) }}" onclick="redirectTo(this)">
+                    <button class="btn btn-light square-btn w-100" data-route="{{ url($button['route']) }}" onclick="redirectTo(this)">
                         <i class="bi bi-{{ $button['icon'] }} icon-size"></i>
                         <div class="label-size">{{ $button['label'] }}</div>
-                    </button>                    
+                    </button>
                 </div>
             @endforeach
         </div>
