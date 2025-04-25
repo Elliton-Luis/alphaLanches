9ab916a2db95322c37760dfc6e319aefdf2fc95c
ArthurThuko <arthurhenriqsfc@gmail.com>
2025-04-25 07:38:03 -0300

    Modificação nos campos de telefone e cpf para sem opcionais

--- app/Livewire/FormCreateAccount.php
+++ app/Livewire/FormCreateAccount.php
@@ -32,27 +32,27 @@ class FormCreateAccount extends Component
     public function storeAccount()
     {
 
-        if(User::where('email',$this->email)->exists()){
-            session()->flash('errorStoreAccount','Email já cadastrado');
-        }elseif(User::where('cpf',$this->cpf)->exists()){
-            session()->flash('errorStoreAccount','CPF já cadastrado');
-        }elseif(User::where('telefone',$this->telefone)->exists()){
-            session()->flash('errorStoreAccount','Telefone já cadastrado');
-        }else{
-            $firstName = strtok($this->name," ");
-            $password = $this->cpf."@".$firstName;
+        if (User::where('email', $this->email)->exists()) {
+            session()->flash('errorStoreAccount', 'Email já cadastrado');
+        } elseif (!empty($this->cpf) && User::where('cpf', $this->cpf)->exists()) {
+            session()->flash('errorStoreAccount', 'CPF já cadastrado');
+        } elseif (!empty($this->telefone) && User::where('telefone', $this->telefone)->exists()) {
+            session()->flash('errorStoreAccount', 'Telefone já cadastrado');
+        } else {
+            $firstName = strtok($this->name, " ");
+            $password = $this->cpf . "@" . $firstName;
 
             User::create([
-                'name'=>$this->name,
-                'email'=>$this->email,
-                'cpf'=>$this->cpf,
-                'telefone'=>$this->telefone,
-                'type'=>$this->type,
-                'password'=>bcrypt($password)
+                'name' => $this->name,
+                'email' => $this->email,
+                'cpf' => $this->cpf,
+                'telefone' => $this->telefone,
+                'type' => $this->type,
+                'password' => bcrypt($password)
             ]);
 
             $this->reset();
-            session()->flash('successStoreAccount','Conta criada com sucesso');
+            session()->flash('successStoreAccount', 'Conta criada com sucesso');
             $this->dispatch('storeAccount');
         }
 
