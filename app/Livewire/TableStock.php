1b129dcb58aefb5fd01743a4fb0cb3e49fff31f1
ArthurThuko <arthurhenriqsfc@gmail.com>
2025-06-23 14:57:47 -0300

    Validação no Backend do Estoque

--- app/Livewire/TableStock.php
+++ app/Livewire/TableStock.php
@@ -18,8 +18,9 @@ class TableStock extends Component
     public $price;
     public $type;
     public $amount;
-    
-    public function mount(){
+
+    public function mount()
+    {
         $this->filterType = null;
         $this->filterName = null;
         $this->name = null;
@@ -32,39 +33,44 @@ class TableStock extends Component
     public function render()
     {
         $products = $this->getProducts();
-        return view('livewire.table-stock',['products'=>$products]);
+        return view('livewire.table-stock', ['products' => $products]);
     }
 
     #[On('alteredProduct')]
     public function getProducts()
     {
         $query = Produto::query();
-        if($this->filterType){
-            $query->where('type',$this->filterType);
+        if ($this->filterType) {
+            $query->where('type', $this->filterType);
         }
 
-        if($this->filterName){
-            $query->where('name','like',"%" . $this->filterName . "%");
+        if ($this->filterName) {
+            $query->where('name', 'like', "%" . $this->filterName . "%");
         }
 
         return $query->paginate(10);
     }
 
-    public function storeProduct(){
+    public function storeProduct()
+    {
         $this->validate([
-            'name' => 'required|unique:products,name',
-            'describe' => 'required|max:255',
-            'price' => 'required|min:0|max:999',
-            'type' => 'required',
+            'name' => 'required|string|max:100|unique:products,name',
+            'describe' => 'required|string|max:255',
+            'price' => 'required|numeric|min:0|max:999.99',
+            'type' => 'required|in:drink,savory,lunch,snacks,natural',
             'amount' => 'required|integer|min:0|max:999'
-        ],[
+        ], [
             'name.required' => 'É Necessário Informar um Nome',
             'name.unique' => 'Produto Já Cadastrado',
             'describe.required' => 'É Necessário Informar uma Descrição',
             'describe.max' => 'Máximo de 255 caracteres',
             'price.required' => 'É Necessário Informar um Preço',
             'type.required' => 'É Necessário Selecionar um Tipo',
-            'amount.required' => 'É Necessário Informar a Quantidade'
+            'amount.required' => 'É Necessário Informar a Quantidade',
+            'price.min' => 'O valor mínimo permitido é 0.',
+            'price.max' => 'O valor máximo permitido é 999.99.',
+            'amount.min' => 'O valor mínimo permitido é 0.',
+            'amount.max' => 'O valor máximo permitido é 999.',
         ]);
 
         Produto::create([
@@ -79,16 +85,18 @@ class TableStock extends Component
         $this->dispatch("closeModal");
     }
 
-    public function addProduct($id){
+    public function addProduct($id)
+    {
         $product = Produto::find($id);
-        $product->amount+=1;
+        $product->amount += 1;
         $product->save();
         $this->dispatch('alteredProduct');
     }
 
-    public function reduceProduct($id){
+    public function reduceProduct($id)
+    {
         $product = Produto::find($id);
-        $product->amount-=1;
+        $product->amount -= 1;
         $product->save();
         $this->dispatch('alteredProduct');
     }
