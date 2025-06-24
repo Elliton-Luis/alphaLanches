772d6a0ec572a1402ddc35f49367d6ead8c82866
ArthurThuko <arthurhenriqsfc@gmail.com>
2025-06-23 21:14:03 -0300

    Criando botão na Sidebar e no painel de botões

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,58 +6,52 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function showHome(){
+    public function showHome()
+    {
         $buttons = $this->checkUserType();
-        return view('home',['buttons' => $buttons]);
+        return view('home', ['buttons' => $buttons]);
     }
 
-    public function checkUserType(){
-        if(auth()->user()->type == 'admin')
-            {
-                return [
-                    ['route' => 'financeiro', 'icon' => 'cash-coin', 'label' => 'Financeiro'],
-                    ['route' => 'estoque', 'icon' => 'box', 'label' => 'Estoque'],
-                    ['route' => 'profile', 'icon' => 'person', 'label' => 'Perfil'],
-                    ['route' => 'painelUsuarios', 'icon' => 'people', 'label' => 'Usuários'],
-                    ['route' => 'painelCompras', 'icon' => 'basket3', 'label' => 'Histórico de Compras'],
-                    ['route' => 'pdv', 'icon' => 'shop', 'label' => 'PDV'],
-                    ['route' => 'recarga', 'icon' => 'wallet', 'label' => 'Recarga'],
-                    ['route' => 'responsaveis', 'icon' => 'journal-plus', 'label' => 'Pedidos de Responsáveis'],
-                    //['route' => 'sobre', 'icon' => 'info-circle', 'label' => 'Sobre Nós'],
-                    ];
-            }
-            elseif (auth()->user()->type == 'func')
-            {
-                return [
-                    ['route' => 'estoque', 'icon' => 'box', 'label' => 'Estoque'],
-                    ['route' => 'profile', 'icon' => 'person', 'label' => 'Perfil'],
-                    ['route' => 'painel.compras', 'icon' => 'basket3', 'label' => 'Histórico de Compras'],
-                    ['route' => 'pdv', 'icon' => 'shop', 'label' => 'PDV'],
-                    //['route' => 'sobre', 'icon' => 'info-circle', 'label' => 'Sobre Nós'],
-                    ];
-            }
-
-            elseif (auth()->user()->type == 'guard')
-            {
-                return [
-                    ['route' => 'profile', 'icon' => 'person', 'label' => 'Perfil'],
-                    ['route' => 'recarga', 'icon' => 'wallet', 'label' => 'Recarga'],
-                    ['route' => 'agendamento', 'icon' => 'calendar-event', 'label' => 'Agendamento'],
-                    ['route' => 'painel.compras', 'icon' => 'basket3', 'label' => 'Histórico de Compras'],
-                    //['route' => 'sobre', 'icon' => 'info-circle', 'label' => 'Sobre Nós'],
-                ];
-            }
-
-            elseif (auth()->user()->type == 'student')
-            {
-                return [
-                    ['route' => 'profile', 'icon' => 'person', 'label' => 'Perfil'],
-                    ['route' => 'agendamento', 'icon' => 'calendar-event', 'label' => 'Agendamento'],
-                    ['route' => 'PainelHistorico', 'icon' => 'basket3', 'label' => 'Histórico de Compras'],
-                    //['route' => 'sobre', 'icon' => 'info-circle', 'label' => 'Sobre Nós'],
-                ];
-            }
-            return [];
+    public function checkUserType()
+    {
+        if (auth()->user()->type == 'admin') {
+            return [
+                ['route' => 'financeiro', 'icon' => 'cash-coin', 'label' => 'Financeiro'],
+                ['route' => 'estoque', 'icon' => 'box', 'label' => 'Estoque'],
+                ['route' => 'profile', 'icon' => 'person', 'label' => 'Perfil'],
+                ['route' => 'painelUsuarios', 'icon' => 'people', 'label' => 'Usuários'],
+                ['route' => 'painelCompras', 'icon' => 'basket3', 'label' => 'Histórico de Compras'],
+                ['route' => 'pdv', 'icon' => 'shop', 'label' => 'PDV'],
+                ['route' => 'recarga', 'icon' => 'wallet', 'label' => 'Recarga'],
+                ['route' => 'responsaveis', 'icon' => 'journal-plus', 'label' => 'Pedidos de Responsáveis'],
+                //['route' => 'sobre', 'icon' => 'info-circle', 'label' => 'Sobre Nós'],
+            ];
+        } elseif (auth()->user()->type == 'func') {
+            return [
+                ['route' => 'estoque', 'icon' => 'box', 'label' => 'Estoque'],
+                ['route' => 'profile', 'icon' => 'person', 'label' => 'Perfil'],
+                ['route' => 'painel.compras', 'icon' => 'basket3', 'label' => 'Histórico de Compras'],
+                ['route' => 'pdv', 'icon' => 'shop', 'label' => 'PDV'],
+                //['route' => 'sobre', 'icon' => 'info-circle', 'label' => 'Sobre Nós'],
+            ];
+        } elseif (auth()->user()->type == 'guard') {
+            return [
+                ['route' => 'profile', 'icon' => 'person', 'label' => 'Perfil'],
+                ['route' => 'painelUsuarios', 'icon' => 'people', 'label' => 'Alunos'],
+                ['route' => 'recarga', 'icon' => 'wallet', 'label' => 'Recarga'],
+                ['route' => 'agendamento', 'icon' => 'calendar-event', 'label' => 'Agendamento'],
+                ['route' => 'painel.compras', 'icon' => 'basket3', 'label' => 'Histórico de Compras'],
+                //['route' => 'sobre', 'icon' => 'info-circle', 'label' => 'Sobre Nós'],
+            ];
+        } elseif (auth()->user()->type == 'student') {
+            return [
+                ['route' => 'profile', 'icon' => 'person', 'label' => 'Perfil'],
+                ['route' => 'agendamento', 'icon' => 'calendar-event', 'label' => 'Agendamento'],
+                ['route' => 'PainelHistorico', 'icon' => 'basket3', 'label' => 'Histórico de Compras'],
+                //['route' => 'sobre', 'icon' => 'info-circle', 'label' => 'Sobre Nós'],
+            ];
+        }
+        return [];
     }
-    
+
 }
--- resources/views/components/sidebarHorizontal.blade.php
+++ resources/views/components/sidebarHorizontal.blade.php
@@ -4,90 +4,97 @@
 
             <!-- Botão Menu -->
             <div class="dropdown">
-                <button class="btn btn-dark border-0 shadow-sm" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <button class="btn btn-dark border-0 shadow-sm" type="button" data-bs-toggle="dropdown"
+                    aria-expanded="false">
                     <i class="bi bi-list fs-3"></i>
                 </button>
                 <ul class="dropdown-menu dropdown-menu-dark text-small shadow rounded-3 mt-2">
                     @if (auth()->user()->type === 'admin')
-                    <li>
-                        <a href="/financeiro" class="dropdown-item btn btn-secondary text-start w-100">
-                            <i class="bi bi-cash-coin me-2"></i> Financeiro
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/painelUsuarios" class="dropdown-item btn btn-secondary text-start w-100 mt-1">
-                            <i class="bi bi-people me-2"></i> Usuários
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/responsaveis/" class="dropdown-item btn btn-secondary text-start w-100 mt-1">
-                            <i class="bi bi-journal-plus me-2"></i> Pedidos
-                        </a>
-                    </li>
+                        <li>
+                            <a href="/financeiro" class="dropdown-item btn btn-secondary text-start w-100">
+                                <i class="bi bi-cash-coin me-2"></i> Financeiro
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/painelUsuarios" class="dropdown-item btn btn-secondary text-start w-100 mt-1">
+                                <i class="bi bi-people me-2"></i> Usuários
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/responsaveis/" class="dropdown-item btn btn-secondary text-start w-100 mt-1">
+                                <i class="bi bi-journal-plus me-2"></i> Pedidos
+                            </a>
+                        </li>
                     @endif
 
                     @if (in_array(auth()->user()->type, ['admin', 'func']))
-                    <li>
-                        <a href="/estoque" class="dropdown-item btn btn-secondary text-start w-100 mt-1">
-                            <i class="bi bi-box me-2"></i> Estoque
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/painelCompras" class="dropdown-item btn btn-secondary text-start w-100 mt-1">
-                            <i class="bi bi-basket3 me-2"></i> Histórico
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/pdv" class="dropdown-item btn btn-secondary text-start w-100 mt-1">
-                            <i class="bi bi-shop me-2"></i> PDV
-                        </a>
-                    </li>
+                        <li>
+                            <a href="/estoque" class="dropdown-item btn btn-secondary text-start w-100 mt-1">
+                                <i class="bi bi-box me-2"></i> Estoque
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/painelCompras" class="dropdown-item btn btn-secondary text-start w-100 mt-1">
+                                <i class="bi bi-basket3 me-2"></i> Histórico
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/pdv" class="dropdown-item btn btn-secondary text-start w-100 mt-1">
+                                <i class="bi bi-shop me-2"></i> PDV
+                            </a>
+                        </li>
                     @endif
 
                     @if (in_array(auth()->user()->type, ['guard']))
-                    <li>
-                        <a href="/painelCompras" class="dropdown-item btn btn-secondary text-start w-100 mt-1">
-                            <i class="bi bi-basket3 me-2"></i> Histórico
-                        </a>
-                    </li>
+                        <li>
+                            <a href="/painelCompras" class="dropdown-item btn btn-secondary text-start w-100 mt-1">
+                                <i class="bi bi-basket3 me-2"></i> Histórico
+                            </a>
+                        </li>
                     @endif
 
                     @if (in_array(auth()->user()->type, ['admin', 'func', 'guard', 'student']))
-                    <li>
-                        <a href="/profile" class="dropdown-item btn btn-secondary text-start w-100 mt-1">
-                            <i class="bi bi-person me-2"></i> Perfil
-                        </a>
-                    </li>
+                        <li>
+                            <a href="/profile" class="dropdown-item btn btn-secondary text-start w-100 mt-1">
+                                <i class="bi bi-person me-2"></i> Perfil
+                            </a>
+                        </li>
                     @endif
 
                     @if (in_array(auth()->user()->type, ['admin', 'guard']))
-                    <li>
-                        <a href="/recarga" class="dropdown-item btn btn-secondary text-start w-100 mt-1">
-                            <i class="bi bi-wallet me-2"></i> Recarga
-                        </a>
-                    </li>
+                        <li>
+                            <a href="/recarga" class="dropdown-item btn btn-secondary text-start w-100 mt-1">
+                                <i class="bi bi-wallet me-2"></i> Recarga
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/painelUsuarios" class="dropdown-item btn btn-secondary text-start w-100 mt-1">
+                                <i class="bi bi-people me-2"></i> Usuários
+                            </a>
+                        </li>
                     @endif
 
                     @if (in_array(auth()->user()->type, ['guard', 'student']))
-                    <li>
-                        <a href="/agendamento" class="dropdown-item btn btn-secondary text-start w-100 mt-1">
-                            <i class="bi bi-calendar-event me-2"></i> Agendamento
-                        </a>
-                    </li>
+                        <li>
+                            <a href="/agendamento" class="dropdown-item btn btn-secondary text-start w-100 mt-1">
+                                <i class="bi bi-calendar-event me-2"></i> Agendamento
+                            </a>
+                        </li>
                     @endif
 
                     @if (auth()->user()->type === 'student')
-                    <li>
-                        <a href="/PainelHistorico" class="dropdown-item btn btn-secondary text-start w-100 mt-1">
-                            <i class="bi bi-basket3 me-2"></i> Histórico
-                        </a>
-                    </li>
+                        <li>
+                            <a href="/PainelHistorico" class="dropdown-item btn btn-secondary text-start w-100 mt-1">
+                                <i class="bi bi-basket3 me-2"></i> Histórico
+                            </a>
+                        </li>
                     @endif
 
                     <hr class="my-2 text-secondary">
 
                     <li>
-                        <form id="logout-form" action="{{ route('login.logout') }}" method="POST" style="display: none;">
+                        <form id="logout-form" action="{{ route('login.logout') }}" method="POST"
+                            style="display: none;">
                             @csrf
                         </form>
                         <a class="dropdown-item btn btn-danger text-start w-100" href="#"
@@ -99,7 +106,8 @@
             </div>
 
             <!-- Logo ou Título -->
-            <a href="{{ route('home') }}" class="text-white text-decoration-none fs-4 fw-bold"><img src="{{ asset('images/AlphaLanches-Logo.png') }}" height="78px" alt="Logo Alpha"></a>
+            <a href="{{ route('home') }}" class="text-white text-decoration-none fs-4 fw-bold"><img
+                    src="{{ asset('images/AlphaLanches-Logo.png') }}" height="78px" alt="Logo Alpha"></a>
         </div>
     </nav>
 </div>
