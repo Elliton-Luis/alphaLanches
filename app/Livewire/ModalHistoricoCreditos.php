0f1147d8e14886076ad87e2078abf1fca6e8c980
ArthurThuko <arthurhenriqsfc@gmail.com>
2025-06-29 10:27:15 -0300

    Criando o liveware de histórico

---
+++ app/Livewire/ModalHistoricoCreditos.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class ModalHistoricoCreditos extends Component
+{
+    public function render()
+    {
+        return view('livewire.modal-historico-creditos');
+    }
+}
